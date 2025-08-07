cceeb0035527741aeaef5044918663f55f98936b
pivolan <pivolan@gmail.com>
2025-08-07 06:09:14 +0400

    Update config.h not tank

--- Inc/config.h
+++ Inc/config.h
@@ -439,17 +439,17 @@
   //#define FILTER                  3000    // 0.1f [-] fixdt(0,16,16) lower value == softer filter [0, 65535] = [0.0 - 1.0].
   ///#define SPEED_COEFFICIENT       16000   // 1.0f [-] fixdt(1,16,14) higher value == stronger. [0, 65535] = [-2.0 - 2.0]. In this case 16384 = 1.0 * 2^14
   //#define STEER_COEFFICIENT       8000   // 1.0f [-] fixdt(1,16,14) higher value == stronger. [0, 65535] = [-2.0 - 2.0]. In this case 16384 = 1.0 * 2^14. If you do not want any steering, set it to 0.
-  #define TANK_STEERING                   // use for tank steering, each input controls each wheel 
+  //#define TANK_STEERING                   // use for tank steering, each input controls each wheel 
   // #define INVERT_R_DIRECTION
   // #define INVERT_L_DIRECTION
   // #define SUPPORT_BUTTONS_LEFT            // use left sensor board cable for button inputs.  Disable DEBUG_SERIAL_USART2!
   // #define SUPPORT_BUTTONS_RIGHT           // use right sensor board cable for button inputs. Disable DEBUG_SERIAL_USART3!
 
-  #if defined(CONTROL_PWM_RIGHT) && !defined(DUAL_INPUTS)
-    #define DEBUG_SERIAL_USART2           // left sensor cable debug
-  #elif defined(CONTROL_PWM_LEFT) && !defined(DUAL_INPUTS)
-    #define DEBUG_SERIAL_USART3           // right sensor cable debug
-  #endif
+  //#if defined(CONTROL_PWM_RIGHT) && !defined(DUAL_INPUTS)
+    //#define DEBUG_SERIAL_USART2           // left sensor cable debug
+  //#elif defined(CONTROL_PWM_LEFT) && !defined(DUAL_INPUTS)
+    //#define DEBUG_SERIAL_USART3           // right sensor cable debug
+  //#endif
 #endif
 // ############################# END OF VARIANT_PWM SETTINGS ############################
 

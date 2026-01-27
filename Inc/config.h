8dd2fd5b8ef8707dc1a6b82d65c0f19bfb098f5f
pivolan <pivolan@gmail.com>
2026-01-27 20:52:20 +0400

    moped v12 front +adc

--- Inc/config.h
+++ Inc/config.h
@@ -230,8 +230,8 @@
   #define DUAL_INPUTS                     // ADC*(Primary) + PWM(Auxiliary). Uncomment this to use Dual-inputs
   #ifdef DUAL_INPUTS
     #define FLASH_WRITE_KEY       0x1106  // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
-    #define CONTROL_ADC           0       // use ADC as input. Number indicates priority for dual-input. Disable CONTROL_SERIAL_USART2, FEEDBACK_SERIAL_USART2, DEBUG_SERIAL_USART2!
-    #define CONTROL_PWM_RIGHT     1       // use RC PWM as input on the RIGHT cable. Number indicates priority for dual-input. Disable DEBUG_SERIAL_USART3!
+    #define CONTROL_ADC           1       // use ADC as input. Number indicates priority for dual-input. Disable CONTROL_SERIAL_USART2, FEEDBACK_SERIAL_USART2, DEBUG_SERIAL_USART2!
+    #define CONTROL_PWM_LEFT     0       // use RC PWM as input on the RIGHT cable. Number indicates priority for dual-input. Disable DEBUG_SERIAL_USART3!
 
     // ADC Triggers: Type 1 = Normal Pot (0 to max range), no deadband needed for triggers
     #define PRI_INPUT1            3,   0, 0, 4095,   0  // Brake trigger:  TYPE, MIN, MID, MAX, DEADBAND. Calibrate MIN/MAX with your triggers!
